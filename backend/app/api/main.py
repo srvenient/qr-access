c852ec5cc227083578beec1ca1150f62b60a4284
srvenient <srvenient@gmail.com>
2025-08-13 16:29:02 -0500

    feat: include role router in main API router alongside user router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.user.infrastructure.http.user_router import router as user_router
+from app.api.role.repository.http.role_routers import router as role_router
+from app.api.user.infrastructure.http.user_routers import router as user_router
 
 api_router = APIRouter()
 
-api_router.include_router(user_router, prefix="/user", tags=["User"])
+api_router.include_router(role_router)
+api_router.include_router(user_router)
