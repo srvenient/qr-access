ea5a2a7e32624d99fa053480542fe3a98ac9e00a
srvenient <srvenient@gmail.com>
2025-08-22 23:56:36 -0500

    feat: enhance authentication flow with login and token refresh endpoints

--- backend/app/api/user/infrastructure/http/auth/auth_routers.py
+++ backend/app/api/user/infrastructure/http/auth/auth_routers.py
@@ -1,12 +1,13 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, Response
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.api.deps import UserAggregateRepositoryDep
 from app.api.shared.aggregate.infrastructure.repository.sql.sql_alchemy_aggregate_root_repository import \
     SQLAlchemyAggregateRootRepository
 from app.api.user.application import auth_services
+from app.api.user.domain.auth_models import Token
 from app.api.user.domain.user_models import UserCreate, User
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -22,7 +23,16 @@ async def register(
 
 @router.post("/login", response_model=auth_services.Token)
 async def login(
+        response: Response,
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
         repository: SQLAlchemyAggregateRootRepository[User] = UserAggregateRepositoryDep
-):
-    return await auth_services.authenticate_user(form_data, repository)
+) -> Token:
+    return await auth_services.authenticate_user(response, form_data, repository)
+
+
+@router.post("/refresh", response_model=auth_services.Token)
+async def refresh_token(
+        request: Request,
+        response: Response,
+) -> Token:
+    return await auth_services.refresh_token(request, response)
