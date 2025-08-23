5e6c8933a5635d35be0bcb685b45e20cf1efef2f
srvenient <srvenient@gmail.com>
2025-08-22 22:34:41 -0500

    feat: implement get_current_user dependency for JWT authentication and error handling

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,13 +1,18 @@
 from typing import Generator, Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from jose import jwt, ExpiredSignatureError, JWTError
 from sqlmodel import Session
+from starlette import status
 
 from app.api.role.domain.role_models import Role
 from app.api.shared.aggregate.infrastructure.repository.sql.sql_alchemy_aggregate_root_repository import \
     SQLAlchemyAggregateRootRepository
 from app.api.user.domain.user_models import User
+from app.core import security
+from app.core.config import settings
 from app.core.db import engine
+from app.core.security import oauth2_scheme
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -36,3 +41,40 @@ def get_user_aggregate_repository(session: SessionDep) -> SQLAlchemyAggregateRoo
 
 
 UserAggregateRepositoryDep = Depends(get_user_aggregate_repository)
+
+
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        repository: SQLAlchemyAggregateRootRepository[User] = UserAggregateRepositoryDep
+) -> User:
+    """
+    Get the current authenticated user from the JWT token.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload: dict = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+            audience=security.ACCESS_AUD,
+        )
+        user_id: str | None = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
+    except JWTError:
+        raise credentials_exception
+
+    user = await repository.find_async(user_id)  # type: ignore
+    if user is None or not user.is_active:
+        raise credentials_exception
+
+    return user
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
