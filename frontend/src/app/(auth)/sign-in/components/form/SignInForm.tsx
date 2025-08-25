5ddec7c1306e1fb81fcb8283ec73e27cb309e1ca
srvenient <srvenient@gmail.com>
2025-08-24 22:40:03 -0500

    feat: implement authentication logic in SignInForm and create AuthHttpClient for API interactions

--- frontend/src/app/(auth)/sign-in/components/form/SignInForm.tsx
+++ frontend/src/app/(auth)/sign-in/components/form/SignInForm.tsx
@@ -4,6 +4,8 @@ import {Form} from '@/app/components/form/Form';
 import {SubmitHandler, useForm} from 'react-hook-form';
 import Input from "@/app/components/form/input/Input";
 import PasswordInput from "@/app/components/form/input/PasswordInput";
+import {useRouter} from "next/navigation";
+import {authHttpClient} from "@/app/(auth)/sign-in/lib/auth.http-client";
 
 type FormValues = {
   username: string;
@@ -18,11 +20,23 @@ export default function SignInForm() {
       password: '',
     },
   });
-  const {formState: {isValid, isSubmitting}} = methods;
+  const {setError, formState: {isValid, isSubmitting}} = methods;
+  const router = useRouter();
 
-  const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
-    console.log('Form data:', data);
-  };
+  const onSubmit: SubmitHandler<FormValues> = async (data: FormValues): Promise<void> => {
+    try {
+      const success = await authHttpClient.login(data.username, data.password);
+      if (success) {
+        router.replace("/dashboard");
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError("username", { message: error.message }); // o "password" según el caso
+      } else {
+        console.error("Unexpected error", error);
+      }
+    }
+  }
 
   return (
     <div className="flex flex-col items-start justify-center w-full gap-8">
@@ -48,7 +62,7 @@ export default function SignInForm() {
               value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
               message: 'Invalid email address',
             },
-        }}
+          }}
         />
         <div className="flex flex-col gap-2">
           <PasswordInput label="Password" placeholder="Your password..."/>
@@ -71,19 +85,20 @@ export default function SignInForm() {
             w-full py-2.5 px-4 mt-2
             bg-theme-dodger-blue enabled:hover:bg-blue-600 
             rounded-xl
-            text-white text-[12px] font-special font-semibold
+            text-white text-[10.5px]  font-semibold
             transition-colors duration-300
             cursor-pointer
             disabled:cursor-not-allowed
           `}
         >
-          Sign In
+          SIGN IN
         </button>
         <div className="flex items-center justify-center gap-1">
           <p className="text-sm text-gray-400">
             By continuing, you agree to our
           </p>
-          <button type="button" className="text-sm text-blue-400 underline hover:text-white transition-colors duration-300 cursor-pointer">
+          <button type="button"
+                  className="text-sm text-blue-400 underline hover:text-white transition-colors duration-300 cursor-pointer">
             terms of service.
           </button>
         </div>
---
+++ frontend/src/app/(auth)/sign-in/lib/auth.http-client.ts
@@ -0,0 +1,45 @@
+import {BaseHttpClient} from "@/common/http-client/http-client";
+import config from "@/config/config";
+import {ApiError} from "@/common/errors/api-error";
+import axios from "axios";
+
+export class AuthHttpClient extends BaseHttpClient {
+  constructor() {
+    super({
+      baseURL: config.clients.fastapi.baseURL,
+      timeout: config.clients.fastapi.timeout,
+      keepAlive: config.clients.fastapi.keepAlive,
+      withCredentials: config.clients.fastapi.withCredentials,
+    });
+  }
+
+  async login(username: string, password: string): Promise<Boolean> {
+    try {
+      await this.instance.post(
+        'auth/login',
+        new URLSearchParams({
+          username,
+          password
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      );
+
+      return true;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        throw new ApiError(
+          error.response.data?.message ?? 'Credentials are not valid',
+          error.response.status
+        );
+      }
+
+      throw new ApiError('Server error', 500);
+    }
+  }
+}
+
+export const authHttpClient = new AuthHttpClient();
